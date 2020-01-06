6a932cb4aa25a8a024c4649880a380dd171546cf
Nikhil Anand <43323927+NikhilAnand1998@users.noreply.github.com>
2020-01-06 13:23:28 -0800

    database functionality setup

--- website/server.js
+++ website/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const testTweets = require('./testTweets.json');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
   user:'admin',
@@ -23,12 +25,34 @@ connection.connect((err) => {
 
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Tweet Labeler',
-    data: testTweets.tweets
+  connection.query('SELECT * FROM data.train_data',(err,rows) => {
+    if(err){
+      console.log('Error selecting tweets from Database...');
+      return;
+    }
+    console.log('Successfully recieved tweets from database');
+     res.render('index', {
+       title: 'Tweet Labeler',
+       data: rows
+     });
   });
+
+
 });
 
+app.post('/',(req,res) => {
+  var newRow = {
+    tweet: req.body.tweet,
+    relevant: req.body.relevant
+  };
+  connection.query('INSERT INTO data.train_data SET ?',newRow,(err,res) => {
+    if(err){
+      console.log("Failed to insert new tweet into database...");
+      return;
+    }
+    console.log("Inserted new Tweet Successfully!");
+  });
+});
 
 const server = app.listen(9000,() => {
    console.log(`Express running → PORT ${server.address().port}`);
