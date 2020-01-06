7c5d4d09380d7903f53a4b4da4ffc6a4f4d6c278
Nikhil Anand <43323927+NikhilAnand1998@users.noreply.github.com>
2020-01-05 18:10:12 -0800

    Add files via upload

--- website/server.js
+++ website/server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 const testTweets = require('./testTweets.json');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+
+const connection = mysql.createConnection({
+  user:'admin',
+  password:'Private2712!',
+  host:'database-1.cok63qqiofsd.us-east-1.rds.amazonaws.com',
+  database:'data'
+});
+
+connection.connect((err) => {
+  if(err){
+    console.log('Error connecting to Database...');
+    return;
+  }
+  console.log('Connected to Database!');
+});
+
 
 
 app.get('/', (req, res) => {
