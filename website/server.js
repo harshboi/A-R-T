7d04e31366d33209fa04b5ae1d08efa7e2033a0a
Nikhil Anand <43323927+NikhilAnand1998@users.noreply.github.com>
2020-01-13 21:55:14 -0800

    Completed Website

--- website/server.js
+++ website/server.js
@@ -27,7 +27,7 @@ connection.connect((err) => {
 
 
 app.get('/', (req, res) => {
-  connection.query("START TRANSACTION; SELECT * FROM data.train_data ORDER BY train_id ASC LIMIT 1; DELETE FROM data.train_data ORDER BY train_id ASC LIMIT 1; COMMIT;",(err,rows) => {
+  connection.query("START TRANSACTION; SELECT * FROM data.train_data ORDER BY train_id ASC LIMIT 1; DELETE FROM data.train_data ORDER BY train_id ASC LIMIT 1; SELECT COUNT(*) FROM data.classified_tweets WHERE relevant = 001; SELECT COUNT(*) FROM data.classified_tweets WHERE relevant = 000; COMMIT;",(err,rows) => {
     if(err){
       console.log('Error selecting tweets from Database...');
       return;
@@ -37,17 +37,18 @@ app.get('/', (req, res) => {
       first = 1;
       res.render('index', {
         title: 'Tweet Labeler',
-        data: rows[1]
+        data: rows[1],
+        relevant_count: rows[3][0]['COUNT(*)'],
+        irrelevant_count: rows[4][0]['COUNT(*)']
       });
     }
     else{
-      res.send({ndata:rows[1]});
+      res.send({ndata:rows[1],rc:rows[3][0]['COUNT(*)'],ic:rows[4][0]['COUNT(*)']});
     }
   });
 
 });
 app.post('/',(req,res) => {
-  console.log(req.body);
   var newRow = {
     created_at: req.body.created_at,
     date: req.body.date,
@@ -85,6 +86,44 @@ app.post('/',(req,res) => {
   connection.query('INSERT INTO data.classified_tweets SET ?',newRow,(err,res) => {
     if(err){
       console.log("Failed to insert new tweet into database...");
+      var reRow = {
+        created_at: req.body.created_at,
+        date: req.body.date,
+        time: req.body.time,
+        timezone: req.body.timezone,
+        user_id: req.body.user_id,
+        username: req.body.username,
+        name: req.body.name,
+        place: req.body.place,
+        tweet: req.body.tweet,
+        mentions:req.body.mentions,
+        urls: req.body.urls,
+        replies_count: req.body.replies_count,
+        retweets_count: req.body.retweets_count,
+        likes_count: req.body.likes_count,
+        hashtags: req.body.hashtags,
+        cashtags:req.body.cashtags,
+        link: req.body.link,
+        retweet: req.body.retweet,
+        video: req.body.video,
+        near: req.body.near,
+        geo: req.body.geo,
+        source: req.body.source,
+        user_rt_id: req.body.user_rt_id,
+        user_rt: req.body.user_rt,
+        retweet_id: req.body.retweet_id,
+        reply_to: req.body.reply_to,
+        retweet_date: req.body.retweet_date,
+        translate:req.body.translate,
+        trans_src:req.body.trans_src,
+        trans_dest: req.body.trans_dest,
+        photos: req.body.photos
+      };
+      connection.query('INSERT INTO data.train_data SET ?',reRow, (err,rest) => {
+        if(!err){
+          console.log("Put the row back in train_data");
+        }
+      });
       return;
     }
     console.log("Inserted new Tweet Successfully!");
